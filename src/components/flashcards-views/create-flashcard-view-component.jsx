f9010b30e4686b4221961e1e2b22aea5c43d91db
scottlurowist <scottlurowist@gmail.com>
2020-09-28 19:37:20 -0400

    THE INITIAL COMMIT FOR THE CREATE FLASHCARD VIEW
    This is the initial commit for the create flashcard view. This will
    be the exemplar for the other views.
    
    Code that was commented in App.js was removed and put into the
    create flashcard view. It is now possible to type into the keyboard
    and see Russian text in the russian textbox. I'll work on styling, and
    I must determine where to do data access.

--- src/components/flashcards-views/create-flashcard-view-component.jsx
+++ src/components/flashcards-views/create-flashcard-view-component.jsx
@@ -8,18 +8,56 @@
 ////////////////////////////////////////////////////////////////////////////////
 
 
-import React from 'react';
+import React, { Component } from 'react';
+
+import InputField from '../input-field/input-field-component';
+import CyrillicKeyboard from '../cyrillic-keyboard/cyrillic-keyboard-component'
 
 
-const CreateFlashcardView = (props) => {
 
-    return(
 
-        <section>
-            <h3>Create Flashcard</h3>
-            <button onClick={() => props.history.push('/options')}>Return</button>
-        </section>
-    );
+// Implements the Create Flashcard view.
+class CreateFlashcardView extends Component {
+
+    constructor(props) {
+        
+        super();
+
+        this.state = {
+            cyrillicInput: ''
+        };
+    }
+
+
+    // A callback method passed to the Cyrillic keyboard component that
+    // sets the state that represents a Russian word typed by the keyboard.
+    // It takes a Cyrillic character and adds it as a suffix to the state
+    // representing that word.
+    //
+    // cyrillicCharacter - a Cyrillic character typed by the CyrillicKeyboard
+    //                     component.
+    // 
+    processKeyboardClick = cyrillicCharacter => {
+
+        this.setState({cyrillicInput: this.state.cyrillicInput + cyrillicCharacter});
+    }
+
+
+    render() {
+        
+        return (
+            <section>
+                <InputField id="create-english-text" label="English"
+                            type="text" placeholder="English" />                
+                <InputField id="create-cyrillic-text" label="русский"
+                            type="text" placeholder="русский"
+                            value={this.state.cyrillicInput}/>
+                <CyrillicKeyboard keyboardPressHandler={ this.processKeyboardClick } />
+
+                <button onClick={() => this.props.history.push('/options')}>Return</button>
+            </section>
+        );
+    }
 }
 
 
