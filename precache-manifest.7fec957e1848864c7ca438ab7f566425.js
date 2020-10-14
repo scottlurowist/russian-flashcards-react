0bf4ad91036bbd60d142a7d9a96f7b11d89f3f4b
scottlurowist <scottlurowist@gmail.com>
2020-10-14 07:08:38 -0400

    Updates

---
+++ precache-manifest.7fec957e1848864c7ca438ab7f566425.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a1e4c5a47b93684aaade013ee251580",
+    "url": "/russian-flashcards-react/index.html"
+  },
+  {
+    "revision": "27ad6a4f33aa6949a33c",
+    "url": "/russian-flashcards-react/static/css/main.faf4bb85.chunk.css"
+  },
+  {
+    "revision": "ad0f1ae5620b7150e699",
+    "url": "/russian-flashcards-react/static/js/2.9b1f8ea8.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/russian-flashcards-react/static/js/2.9b1f8ea8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ad6a4f33aa6949a33c",
+    "url": "/russian-flashcards-react/static/js/main.a0842c19.chunk.js"
+  },
+  {
+    "revision": "555618fae5005053cfda",
+    "url": "/russian-flashcards-react/static/js/runtime-main.269a2335.js"
+  },
+  {
+    "revision": "03b322f3c0c4d98177cf93b149a6430d",
+    "url": "/russian-flashcards-react/static/media/St.Basils_Cathedral_Moscow.03b322f3.jpg"
+  }
+]);
